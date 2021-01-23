e82149c60d398afafb91d71c449f61e80fdf86e1
diohabara <diohabara@gmail.com>
2021-01-24 08:23:16 +0900

    fix: delete fly

--- dotfiles/.config/doom/packages.el
+++ dotfiles/.config/doom/packages.el
@@ -12,7 +12,6 @@
 (when (file-exists-p "~/.wakatime.cfg")
   (package! wakatime-mode))
 (package! slime)
-(package! sly)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
