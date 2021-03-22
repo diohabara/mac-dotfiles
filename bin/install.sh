cb7464ad64a37daf453b36c1c823d123bcd4b61b
diohabara <diohabara@gmail.com>
2021-03-22 13:04:23 +0900

    fix: correct syntax

--- bin/install.sh
+++ bin/install.sh
@@ -122,7 +122,7 @@ function already() {
       pip3 install isort
       pip3 install python-language-server[all]
       pip3 install online-judge-tools
-      pip3 atcoder-tools
+      pip3 install atcoder-tools
     fi
   }
 }
