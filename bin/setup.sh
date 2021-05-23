c3039fcc3281bfaacd6c64ef4772add285a3d384
diohabara <diohabara@gmail.com>
2021-05-23 20:49:58 +0900

    fix: correct syntax error

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,9 +12,9 @@ command_exists() {
 	command -v "$1" >/dev/null 2>&1
 }
 
-if [ "$(uname)" = "Darwin" ] && [ ! command_exists xcode-select ]; then
+if [ "$(uname)" = "Darwin" ] && ! command_exists xcode-select; then
 	xcode-select --install
-elif [ -e /etc/lsb-release ] && [ ! command_exists git ]; then
+elif [ -e /etc/lsb-release ] && ! command_exists git; then
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install -y git
