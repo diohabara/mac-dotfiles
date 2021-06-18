d95ebea21c1cc3b223b710c44946c868848c2f02
diohabara <diohabara@gmail.com>
2021-06-18 17:44:18 +0900

    fix: categorize packages

--- dotfiles/.config/nixpkgs/config.nix
+++ dotfiles/.config/nixpkgs/config.nix
@@ -9,9 +9,46 @@ in {
       myPackages = pkgs.buildEnv {
         name = "my-packages";
         paths = [
+          # node packages
+          nodePackages.bash-language-server
+          nodePackages.js-beautify
+          nodePackages.npm
+          nodePackages.prettier
+          nodePackages.pyright
+          nodePackages.stylelint
+          nodejs
+          # Python packages
+          python38Packages.python-language-server
+          python39
+          python39Packages.isort
+          python39Packages.nose
+          python39Packages.pip
+          python39Packages.pyflakes
+          python39Packages.pytest
+          pipenv
+          # Rust packages
           alacritty
-          aspell
           bat
+          exa
+          fd
+          hyperfine
+          tokei
+          procs
+          # Go packages
+          fzf
+          ghq
+          go
+          gocode
+          golangci-lint
+          gomodifytags
+          gore
+          gotests
+          # Haskell packages
+          ghc
+          haskellPackages.ghcup
+          stack
+          # others
+          aspell
           bazel
           bazel-buildtools
           black
@@ -24,31 +61,18 @@ in {
           dust
           editorconfig-checker
           editorconfig-core-c
-          exa
           expat
-          fd
           ffmpeg
           flock
-          fzf
           gawk
           gdb
           gh
-          ghc
-          ghq
           gibo
           git
           gmp
           gnused
-          go
-          gocode
-          golangci-lint
-          gomodifytags
-          gore
-          gotests
           gradle
           gzip
-          haskellPackages.ghcup
-          hyperfine
           isl
           jq
           libmpc
@@ -58,27 +82,11 @@ in {
           neofetch
           neovim
           nixfmt
-          nodePackages.bash-language-server
-          nodePackages.js-beautify
-          nodePackages.npm
-          nodePackages.prettier
-          nodePackages.pyright
-          nodePackages.stylelint
-          nodejs
           opam
           openssl
           pandoc
-          pipenv
           pkg-config
           poetry
-          procs
-          python38Packages.python-language-server
-          python39
-          python39Packages.isort
-          python39Packages.nose
-          python39Packages.pip
-          python39Packages.pyflakes
-          python39Packages.pytest
           readline
           ripgrep
           rlwrap
@@ -90,13 +98,11 @@ in {
           sd
           shellcheck
           shfmt
-          stack
           starship
           tcl
           tectonic
           tldr
           tmux
-          tokei
           typora
           unzip
           wakatime
