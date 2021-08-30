379c5605516dc5573d701c92684b711c078bf388
diohabara <diohabara@gmail.com>
2021-08-30 15:09:37 +0900

    fix: check directory

--- bin/install.sh
+++ bin/install.sh
@@ -89,7 +89,7 @@ esac
 }
 
 : "install zplug" && {
-  if ! command_exists zplug; then
+  if ! [ -d "${HOME}/.zplug" ]; then
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
   fi
 }
