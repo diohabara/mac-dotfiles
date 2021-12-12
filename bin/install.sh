923a75c86f1e0f74ddb4e47065b7bf9f11d70201
diohabara <diohabara@gmail.com>
2021-12-12 10:38:51 +0900

    fix: not install Doom

--- bin/install.sh
+++ bin/install.sh
@@ -76,14 +76,14 @@ linux*)
   ;;
 esac
 
-: "install Doom Emacs" && {
-  if ! [ -d "$HOME/.emacs.d" ]; then
-    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-    ~/.emacs.d/bin/doom install
-  else
-    ~/.emacs.d/bin/doom sync
-  fi
-}
+#: "install Doom Emacs" && {
+#  if ! [ -d "$HOME/.emacs.d" ]; then
+#    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+#    ~/.emacs.d/bin/doom install
+#  else
+#    ~/.emacs.d/bin/doom sync
+#  fi
+#}
 
 : "install zplug" && {
   if ! [ -d "${HOME}/.zplug" ]; then
