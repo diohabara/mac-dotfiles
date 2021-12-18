73a14e684f1c5456591b53e6390afe0bb3f6b19c
diohabara <diohabara@gmail.com>
2021-12-18 17:20:41 +0900

    refactor: solve SC2038

--- bin/link.sh
+++ bin/link.sh
@@ -11,8 +11,8 @@ cd "${DOTFILES_HOME}"
 
 echo 'Symlinking dotfiles...'
 if [[ $OSTYPE == 'darwin'* ]]; then
-  find . -type d | xargs -I '{}' mkdir -p "${HOME}/{}"
-  find . -type f | xargs -I '{}' ln -snf "${DOTFILES_HOME}/{}" "${HOME}/{}"
+  find . -type d -print0 | xargs -0 -I '{}' mkdir -p "${HOME}/{}"
+  find . -type f -print0 | xargs -0 -I '{}' ln -snf "${DOTFILES_HOME}/{}" "${HOME}/{}"
 else
   find . -type d -print0 | xargs --null -I '{}' mkdir -p "${HOME}/{}"
   find . -type f -print0 | xargs --null -I '{}' ln -snf "${DOTFILES_HOME}/{}" "${HOME}/{}"
