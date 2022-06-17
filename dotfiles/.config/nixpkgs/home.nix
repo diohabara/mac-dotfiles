eba88ca8871ea3c96974494d849a86f7590df2a0
tuampon <diohabara@gmail.com>
2022-06-17 11:21:21 +0900

    fix: remove some packages

--- dotfiles/.config/nixpkgs/home.nix
+++ dotfiles/.config/nixpkgs/home.nix
@@ -27,23 +27,15 @@ in
       })
 
       # node packages
-      #nodePackages.bash-language-server
-      #nodePackages.js-beautify
-      #nodePackages.npm
+      nodePackages.bash-language-server
       nodePackages.prettier
-      nodePackages.pyright
-      #nodePackages.stylelint
-      #nodejs
 
       # Python packages
-      #pipenv
       poetry
       python310Packages.python-lsp-server
       python310Packages.isort
-      #python310Packages.nose
       python310Packages.pip
-      #python310Packages.pyflakes
-      #python310Packages.pytest
+      black
       wakatime
 
       # Rust packages
@@ -51,42 +43,17 @@ in
       bat
       bottom
       delta
-      #du-dust
       exa
       fd
       hyperfine
-      mdbook
-      #procs
       ripgrep
-      #rust-analyzer
-      #sd
+      rust-analyzer
       starship
       tectonic
-      #tidy-viewer
-      #tokei
-      #zellij
-      #zoxide
 
       # Go packages
       fzf
       ghq
-      go
-      #gocode
-      #golangci-lint
-      #gomodifytags
-      #gore
-      #gotests
-
-      # Haskell packages
-      haskellPackages.ghc
-      #haskellPackages.ghcup
-      haskellPackages.haskell-language-server
-      haskellPackages.hoogle
-      haskellPackages.pandoc
-      haskellPackages.stack
-
-      # OCaml package
-      opam
 
       # Git tools
       act
@@ -98,52 +65,31 @@ in
       neovim
 
       # others
-      #bazel
-      #bazel-buildtools
-      #binutils
-      #ccls
-      #clang-tools
-      #jdk11
-      #jdk8
-      #reattach-to-user-namespace
-      #aspell
-      #bison
-      black
-      #broot
+      binutils
+      ccls
+      clang-tools
       cachix
       cmake
-      #coq
-      #coreutils
+      coreutils
       curl
       docker
       docker-compose
       editorconfig-checker
       editorconfig-core-c
-      expat
       ffmpeg
       findutils
-      flock
-      fontconfig
       gawk
       gdb
-      #gibo
-      #glslang
       gmp
       gnused
-      gradle
-      #graphviz
       gzip
-      #hugo
       imagemagick
-      inkscape
       isl
-      jq
       libmpc
       libtelnet
       llvm
-      #mpfr
+      mpfr
       neofetch
-      ninja
       nixfmt
       nixpkgs-fmt
       openssl
@@ -151,21 +97,14 @@ in
       readline
       rlwrap
       rnix-lsp
-      #roswell
-      #rsync
-      #rufo
       shellcheck
       shfmt
-      #tcl
       texlive.combined.scheme-minimal
-      #tikzit
       tldr
       tmux
       unzip
-      verilog
       wget
       xz
-      yarn
       yt-dlp
       zlib
     ];
